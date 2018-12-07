cce36650bda1c2c89f78345bd61b8c037459423a
Joey <joey91133@gmail.com>
2018-12-07 18:21:09 +0800

    change viewerjs to own version.

--- dist/v-viewer.js
+++ dist/v-viewer.js
@@ -222,7 +222,7 @@ var Component = __webpack_require__(8)(
   /* cssModules */
   null
 )
-Component.options.__file = "D:\\Workspaces\\Web\\Git\\v-viewer\\src\\component.vue"
+Component.options.__file = "C:\\Users\\Joey\\Documents\\v-viewer\\src\\component.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] component.vue: functional components are not supported with templates, they should use render functions.")}
 
